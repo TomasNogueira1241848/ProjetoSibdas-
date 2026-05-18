01fc3c2cdad72f717b930a30ea8d59a8db3aaff4
TomásNogueira1241848 <1241848@isep.ipp.pt>
2026-05-18 16:19:12 +0100

    Criação do Favicon e implementação de JS para validação do formulário de Contacto

---
+++ assets/js/1241848.js
@@ -0,0 +1,17 @@
+const formContacto = document.getElementById ('formContacto');
+if (formContacto) {
+    formContacto.addEventListener('submit', function (e){
+        e.preventDefault();
+        if(!formContacto.checkValidity()){
+            e.stopPropagation();
+            formContacto.classList.add('was-validated');
+            return;
+        }
+        formContacto.classList.remove('was-validated');
+        formContacto.reset();
+        document.getElementById('mensagemSucesso').classList.remove('d-none');
+        setTimeout(() => {
+            document.getElementById('mensagemSucesso').classList.add('d-none');
+            }, 4000);
+    });
+}
